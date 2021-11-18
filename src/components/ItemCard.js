dee7dad88636b62e2877d76779bc2d1bc6500d53
Max Mezalon <jmezalon@gmail.com>
2021-11-18 10:33:15 -0500

    basic css

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,44 +1,58 @@
-import {useState} from "react"
+import { useState } from "react";
 
-function ItemCard({item,activeBag,setActiveBag,activeBagNum,activeUser,setActiveUser}){
+function ItemCard({
+  item,
+  activeBag,
+  setActiveBag,
+  activeBagNum,
+  activeUser,
+  setActiveUser,
+}) {
+  const [inBag, setInBag] = useState(false);
 
-const [inBag,setInBag]=useState(false)
+  function handlePacked(e) {
+    setInBag(!inBag);
+  }
 
-function handlePacked(e){
-    setInBag(!inBag)
-}
-
-function handleDelete(e){
-    const bagMinusDelete=[...activeBag].filter((givenItem) => {
-        return (givenItem !==item)
-    })
-    setActiveBag(bagMinusDelete)
-    const currentItemsLocation=`items${activeBagNum}`
-    const activeUserID=activeUser.id
-    fetch(`http://localhost:3000/users/${activeUserID}`,{
+  function handleDelete(e) {
+    const bagMinusDelete = [...activeBag].filter((givenItem) => {
+      return givenItem !== item;
+    });
+    setActiveBag(bagMinusDelete);
+    const currentItemsLocation = `items${activeBagNum}`;
+    const activeUserID = activeUser.id;
+    fetch(`http://localhost:3000/users/${activeUserID}`, {
       method: "PATCH",
       headers: {
-        'Content-Type':'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        [currentItemsLocation]:bagMinusDelete
-      })
+        [currentItemsLocation]: bagMinusDelete,
+      }),
     })
-    .then((r) => r.json())
-    .then((data) => {
-      fetch(`http://localhost:3000/users/${activeUserID}`)
       .then((r) => r.json())
-      .then((data) => setActiveUser(data))
-    })
+      .then((data) => {
+        fetch(`http://localhost:3000/users/${activeUserID}`)
+          .then((r) => r.json())
+          .then((data) => setActiveUser(data));
+      });
+  }
+
+  return (
+    <tr>
+      <td>
+        <button onClick={handleDelete}>♺</button>
+      </td>
+      <td>{item}</td>
+      <td>
+        {inBag ? (
+          <button onClick={handlePacked}>Packed</button>
+        ) : (
+          <button onClick={handlePacked}>Unpacked</button>
+        )}
+      </td>
+    </tr>
+  );
 }
 
-    return (
-        <tr>
-        <td><button onClick={handleDelete}>X</button></td>
-        <td>{item}</td>
-        <td>{inBag ? <button onClick={handlePacked}>Packed</button> : <button onClick={handlePacked}>Unpacked</button>}</td>
-        </tr>
-    )
-}
-
-export default ItemCard
+export default ItemCard;
